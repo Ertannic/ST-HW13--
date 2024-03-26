f907a3297ee4302294b80d07443ce850146c5d87
Erzhan <saralaev03@mail.ru>
2024-03-26 21:24:52 +0600

    Add classic settings

---
+++ ST-HW13- Саралаев Ержан/Controllers/app/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  ST-HW13- Саралаев Ержан
+//
+//  Created by Ertannic Saralay on 26.03.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+}
+
