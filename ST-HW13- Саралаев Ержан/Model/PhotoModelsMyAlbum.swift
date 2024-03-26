8ce4af30faa406a950adbd98f9a7d746fd483697
Erzhan <saralaev03@mail.ru>
2024-03-26 21:53:32 +0600

    Add models/files

--- ST-HW13- Саралаев Ержан/Model/PhotoModelsMyAlbum.swift
+++ ST-HW13- Саралаев Ержан/Model/PhotoModelsMyAlbum.swift
@@ -2,6 +2,10 @@ import Foundation
 
 struct Model {
     static var images: [String] = [
-    "bo1", "bo2", "bo3", "bo4", "bo5", "bo6", "bo7", "bo8", "bo9", "bo10", "bo11", "bo12", "bo13", "bo14", "bo15", "bo16", "bo17"
+        
+    "bo1", "bo2", "bo3", "bo4", "bo5", "bo6", "bo7", "bo8", 
+    "bo9", "bo10", "bo11", "bo12", "bo13", "bo14", "bo15",
+    "bo16", "bo17"
+    
     ]
 }
---
+++ ST-HW13- Саралаев Ержан/Model/myAlbumsTitle.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+struct AlbumData {
+    static let myAlbums = [ "Недавние", "Тренировки", "Коротыш", "WhatsApp", "Snapchat",
+                            "CapCut", "Избранные", "Педро", "Instagram", "Pinterest",
+                            "Prequerl", "Instagram" ]
+}
